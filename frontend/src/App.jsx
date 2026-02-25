179c24ae22d6203c9f0a912d86f450bee0f69309
Md Sayed Uddin <sayedrayhan10@gmail.com>
2026-02-26 01:52:14 +0600

    Setup frontend with react and tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='border border-black p-4 m-4'>LMS</div>
+      
+    </>
+  )
+}
+
+export default App
