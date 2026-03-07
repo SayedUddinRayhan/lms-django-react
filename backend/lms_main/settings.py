6347d59dae5d81dd386f76a513a25efc102dc1fd
Sayed <sayedrayhan10@gmail.com>
2026-03-07 19:21:13 +0600

    Changes on settings file for deployment

--- backend/lms_main/settings.py
+++ backend/lms_main/settings.py
@@ -27,7 +27,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env.bool("DEBUG", default=False)
 SECRET_KEY = env.str("SECRET_KEY")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "lms-django-react.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1",
+    "localhost",
+    "lms-django-react-fy1a.onrender.com",
+    ".onrender.com",
+    ]
 
 
 # Application definition
@@ -66,6 +70,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "lms-django-react.vercel.app",
 ]
 
 # CORS_ALLOW_CREDENTIALS = True
