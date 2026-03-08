634e45af143da5677d2e8d728a735f7484af3acc
Sayed <sayedrayhan10@gmail.com>
2026-03-08 14:36:33 +0600

    fix cloudinary storage

--- backend/lms_main/settings.py
+++ backend/lms_main/settings.py
@@ -207,4 +207,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
